8393ca8fdacb91eea44538bba784351c88b3e87c
Erdem <erdemsincer123@icloud.com>
2025-06-18 01:50:30 +0300

    Add placeholder Persistence folder to User.Infrastructure

---
+++ services/UserService/NeuroSuite.User.Infrastructure/Persistence/UserDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NeuroSuite.User.Domain.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace NeuroSuite.User.Infrastructure.Persistence;
+
+public class UserDbContext : DbContext
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+    public DbSet<UserProfile> UserProfiles => Set<UserProfile>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserProfile>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.Email).IsUnique();
+            entity.Property(x => x.Email).IsRequired().HasMaxLength(150);
+            entity.Property(x => x.FullName).IsRequired().HasMaxLength(150);
+            entity.Property(x => x.Role).IsRequired().HasMaxLength(50);
+        });
+    }
+}
