636e1e31bec9e2f3638ddf238a5ec2b5ebfd490f
Erdem <erdemsincer123@icloud.com>
2025-06-18 01:26:25 +0300

    Add AuthenticationResult DTO to return authentication outcome

---
+++ services/AuthService/NeuroSuite.Auth.Application/Common/AuthenticationResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroSuite.Auth.Application.Common
+{
+    internal class AuthenticationResult
+    {
+    }
+}
