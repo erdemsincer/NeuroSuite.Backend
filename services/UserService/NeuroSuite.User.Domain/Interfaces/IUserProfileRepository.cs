f25570962ddc80ef47e114d98587c9e325cb56dc
Erdem <erdemsincer123@icloud.com>
2025-06-18 01:50:30 +0300

    Add IUserProfileRepository interface to User.Domain

---
+++ services/UserService/NeuroSuite.User.Domain/Interfaces/IUserProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿using NeuroSuite.User.Domain.Entities;
+
+namespace NeuroSuite.User.Domain.Interfaces;
+
+public interface IUserProfileRepository
+{
+    Task AddAsync(UserProfile user);
+    Task<UserProfile?> GetByIdAsync(Guid id);
+    Task<UserProfile?> GetByEmailAsync(string email);
+}
