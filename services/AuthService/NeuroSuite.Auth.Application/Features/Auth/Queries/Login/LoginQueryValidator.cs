86d71e5c8f23b83f9913ab749b8cb85c318dde99
Erdem <erdemsincer123@icloud.com>
2025-06-18 01:27:03 +0300

    Add LoginQueryValidator for validating user login input

---
+++ services/AuthService/NeuroSuite.Auth.Application/Features/Auth/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroSuite.Auth.Application.Features.Auth.Queries.Login
+{
+    internal class LoginQueryValidator
+    {
+    }
+}
