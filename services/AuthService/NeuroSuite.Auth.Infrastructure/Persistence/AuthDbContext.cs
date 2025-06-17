6c19d09a47abba2d73ba342881adc0456a9541b3
Erdem <erdemsincer123@icloud.com>
2025-06-18 01:39:36 +0300

    Add AuthDbContext with Users DbSet

---
+++ services/AuthService/NeuroSuite.Auth.Infrastructure/Persistence/AuthDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NeuroSuite.Auth.Domain.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace NeuroSuite.Auth.Infrastructure.Persistence;
+
+public class AuthDbContext : DbContext
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(u => u.Id);
+            entity.HasIndex(u => u.Email).IsUnique();
+            entity.Property(u => u.FirstName).IsRequired().HasMaxLength(100);
+            entity.Property(u => u.LastName).IsRequired().HasMaxLength(100);
+            entity.Property(u => u.Email).IsRequired().HasMaxLength(150);
+            entity.Property(u => u.PasswordHash).IsRequired();
+            entity.Property(u => u.Role).IsRequired();
+        });
+    }
+}
