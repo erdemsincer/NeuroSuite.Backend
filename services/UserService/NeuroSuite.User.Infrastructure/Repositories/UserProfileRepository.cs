5b57f4163251fd887607f85d90400f1a5e2f51d3
Erdem <erdemsincer123@icloud.com>
2025-06-18 01:50:30 +0300

    Add UserProfileRepository implementation to Infrastructure layer

---
+++ services/UserService/NeuroSuite.User.Infrastructure/Repositories/UserProfileRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NeuroSuite.User.Domain.Entities;
+using NeuroSuite.User.Domain.Interfaces;
+using NeuroSuite.User.Infrastructure.Persistence;
+
+namespace NeuroSuite.User.Infrastructure.Repositories;
+
+public class UserProfileRepository : IUserProfileRepository
+{
+    private readonly UserDbContext _context;
+
+    public UserProfileRepository(UserDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(UserProfile user)
+    {
+        await _context.UserProfiles.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<UserProfile?> GetByIdAsync(Guid id)
+    {
+        return await _context.UserProfiles.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<UserProfile?> GetByEmailAsync(string email)
+    {
+        return await _context.UserProfiles.FirstOrDefaultAsync(x => x.Email == email);
+    }
+}
