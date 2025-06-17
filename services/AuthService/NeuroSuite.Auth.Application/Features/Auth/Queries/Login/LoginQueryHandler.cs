2221c3c66a44a9bc321fe9459697c550250ba4b0
Erdem <erdemsincer123@icloud.com>
2025-06-18 01:39:35 +0300

    Implement LoginQueryHandler with user validation and token return

--- services/AuthService/NeuroSuite.Auth.Application/Features/Auth/Queries/Login/LoginQueryHandler.cs
+++ services/AuthService/NeuroSuite.Auth.Application/Features/Auth/Queries/Login/LoginQueryHandler.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.AspNet.Identity;
+using NeuroSuite.Auth.Application.Common;
+using NeuroSuite.Auth.Domain.Interfaces;
+using IPasswordHasher = NeuroSuite.Auth.Domain.Interfaces.IPasswordHasher;
 
-namespace NeuroSuite.Auth.Application.Features.Auth.Queries.Login
+namespace NeuroSuite.Auth.Application.Features.Auth.Queries.Login;
+
+public class LoginQueryHandler : IRequestHandler<LoginQuery, AuthenticationResult>
 {
-    internal class LoginQueryHandler
+    private readonly IUserRepository _userRepository;
+    private readonly IJwtProvider _jwtProvider;
+    private readonly IPasswordHasher _passwordHasher;
+
+    public LoginQueryHandler(
+        IUserRepository userRepository,
+        IJwtProvider jwtProvider,
+        IPasswordHasher passwordHasher)
     {
+        _userRepository = userRepository;
+        _jwtProvider = jwtProvider;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<AuthenticationResult> Handle(LoginQuery request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByEmailAsync(request.Email);
+        if (user is null)
+            throw new Exception("E-posta veya şifre hatalı.");
+
+        var isValid = _passwordHasher.VerifyPassword(request.Password, user.PasswordHash);
+        if (!isValid)
+            throw new Exception("E-posta veya şifre hatalı.");
+
+        var token = _jwtProvider.GenerateToken(user);
+
+        return new AuthenticationResult
+        {
+            Token = token,
+            Email = user.Email,
+            FullName = $"{user.FirstName} {user.LastName}"
+        };
     }
 }
