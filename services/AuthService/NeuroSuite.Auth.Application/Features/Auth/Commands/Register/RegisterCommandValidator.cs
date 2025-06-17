b18f67c3fd0b7d8074b09580f47509db4eabee36
Erdem <erdemsincer123@icloud.com>
2025-06-18 01:39:35 +0300

    Add RegisterCommandValidator using FluentValidation

--- services/AuthService/NeuroSuite.Auth.Application/Features/Auth/Commands/Register/RegisterCommandValidator.cs
+++ services/AuthService/NeuroSuite.Auth.Application/Features/Auth/Commands/Register/RegisterCommandValidator.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
-namespace NeuroSuite.Auth.Application.Features.Auth.Commands.Register
+namespace NeuroSuite.Auth.Application.Features.Auth.Commands.Register;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
 {
-    internal class RegisterCommandValidator
+    public RegisterCommandValidator()
     {
+        RuleFor(x => x.FirstName).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.LastName).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(6);
     }
 }
