94f2b36ec969225aef18891192f7855abee5ffd7
Erdem <erdemsincer123@icloud.com>
2025-06-18 01:39:36 +0300

    Add PasswordHasher implementation using ASP.NET Identity

---
+++ services/AuthService/NeuroSuite.Auth.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,42 @@
+﻿using NeuroSuite.Auth.Domain.Interfaces;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNet.Identity;
+
+namespace NeuroSuite.Auth.Infrastructure.Security;
+
+public class PasswordHasher : Domain.Interfaces.IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        byte[] salt = new byte[128 / 8];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(salt);
+
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+        return $"{Convert.ToBase64String(salt)}.{hashed}";
+    }
+
+    public bool VerifyPassword(string password, string hashedPassword)
+    {
+        var parts = hashedPassword.Split('.');
+        if (parts.Length != 2)
+            return false;
+
+        var salt = Convert.FromBase64String(parts[0]);
+        var hash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+        return hash == parts[1];
+    }
+}
