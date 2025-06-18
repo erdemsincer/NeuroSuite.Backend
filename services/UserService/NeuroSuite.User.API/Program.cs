1412999cdab7243883a2f8b088e9369e7005a2aa
Erdem <erdemsincer123@icloud.com>
2025-06-18 03:26:33 +0300

    Update User.API files

--- services/UserService/NeuroSuite.User.API/Program.cs
+++ services/UserService/NeuroSuite.User.API/Program.cs
@@ -2,14 +2,19 @@ using Microsoft.EntityFrameworkCore;
 using NeuroSuite.User.Infrastructure.Persistence;
 using NeuroSuite.User.Domain.Interfaces;
 using NeuroSuite.User.Infrastructure.Repositories;
+using NeuroSuite.User.Infrastructure.Messaging;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// PostgreSQL
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Dependency Injection
 builder.Services.AddScoped<IUserProfileRepository, UserProfileRepository>();
+builder.Services.AddSingleton<RabbitMQConsumer>();
 
+// Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,7 +23,20 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+// RabbitMQ listener baþlat
+using (var scope = app.Services.CreateScope())
+{
+    var consumer = scope.ServiceProvider.GetRequiredService<RabbitMQConsumer>();
+    Task.Run(() => consumer.Start());
+}
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+    db.Database.Migrate();
+}
 app.Run();
