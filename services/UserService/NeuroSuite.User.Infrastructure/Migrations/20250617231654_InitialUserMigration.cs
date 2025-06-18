1ae9aa31f620d3b6d0815ba7183c5d0925d9e339
Erdem <erdemsincer123@icloud.com>
2025-06-18 03:17:31 +0300

    Add initial migration files for UserDbContext

---
+++ services/UserService/NeuroSuite.User.Infrastructure/Migrations/20250617231654_InitialUserMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NeuroSuite.User.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUserMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    FullName = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Role = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_Email",
+                table: "UserProfiles",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+        }
+    }
+}
